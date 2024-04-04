f3143993823e0d88c99bf3129cb034c8a971ecf5
TheMrThx <danielodonia@gmail.com>
2024-04-04 22:54:33 +0200

    Add project files.

---
+++ RCP_Klient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RCP_Klient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
